447862a1d1ea9f8d418f67e5f717994c9c31bea0
Karan Patel <karanpatel7862@gmail.com>
2024-08-19 12:55:03 -0600

    commit
    commit

---
+++ speechrecognizegujarati.py
@@ -0,0 +1,26 @@
+from types import resolve_bases
+import speech_recognition as sr
+from googletrans import Translator
+
+translator=Translator()
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said in Gujarati : {}".format(text))
+        result=translator.translate(text,src='gu',dest='en')
+        print('Your Result in English:{}'.format(result.text))
+    except:
+        print("Sorry could not recognize what you said")
+
+
+# from googletrans import Translator
+
+# translator = Translator()
+# result = translator.translate('Mikä on nimesi', src='fi', dest='fr')
+
+# print(result.src)
+# print(result.dest)
+# print(result.text)
